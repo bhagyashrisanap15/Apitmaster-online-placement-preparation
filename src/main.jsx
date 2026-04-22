4943a02816e4bbdfab1af245d777f0c783fcebeb
khushabudeore22 <khushideore2212@gmail.com>
2026-04-22 21:34:27 +0530

    Project added in github

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import toast from 'react-hot-toast';
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx';
+import './index.css';
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 root.render(
-)
+  <BrowserRouter>
+    <App />
+    <Toaster position="top-center" />
+  </BrowserRouter>
+);
