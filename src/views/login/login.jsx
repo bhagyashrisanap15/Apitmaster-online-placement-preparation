abd5f45af9158ddcba022034c27684a3fef986ec
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-26 22:02:37 +0530

    create right side

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -6,3 +6,54 @@ export default function Login() {
     email: "",
     password: "",
   });
+
+  
+  return (
+    <div className="wrapper">
+      <div className="container">
+
+        {/* LEFT SIDE */}
+        <div className="left">
+          <h2 className="heading-container">Login</h2>
+          <p>Enter to improve your sleep and bring peace to your life</p>
+
+          {/* Google */}
+          <a
+            href="https://accounts.google.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="social-btn"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/281/281764.png"
+              alt="Google"
+              className="icon"
+            />
+            Sign in with Google
+          </a>
+
+          {/* Facebook */}
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="social-btn"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/733/733547.png"
+              alt="Facebook"
+              className="icon"
+            />
+            Sign in with Facebook
+          </a>
+
+
+           
+        </div>
+
+       
+
+      </div>
+    </div>
+  );
+}
